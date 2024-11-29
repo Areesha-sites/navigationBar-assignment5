a92f31c919f3705ed56b60a467c88867848ad934
Areesha-sites <areesha21314@gmail.com>
2024-11-29 18:54:41 +0500

    something changed

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -16,9 +16,9 @@ const Navbar = () => {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <a href="/" className="text-white">
+                <Link href="/" className="text-white">
                   Logo
-                </a>
+                </Link>
               </div>
             </div>
 
